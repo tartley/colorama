tartley/colorama

commit 8cc97352831ba804cd55eec408b088553e0245d4

Fix issue #47, incompatible with pyreadline.
Use the fix supplied by google code user eryksun,
creating our own instance of WinDLL('kernel32'),
since this seems to make sure we won't be incompatible
with any other libraries that also use ctypes.windll.

Still haven't tested that this fix is safe on non-win platforms...

colorama/win32.py
@@ -4,8 +4,12 @@
 STDOUT = -11
 STDERR = -12
 
+import ctypes
+from ctypes import LibraryLoader
+
+windll = LibraryLoader(ctypes.WinDLL)
+
 try:
-    from ctypes import windll
     from ctypes import wintypes
 except ImportError:
     windll = None

commit c83445acd79684e4cb38f0c1f4c51698a039109a

fix issue #56 - 64-bit CPython 2.5 does not include ctypes on Windows
The issue was originally introduced in commit
8cc97352831ba804cd55eec408b088553e0245d4 which resolved issue #47 (pyreadline
incompatibility) but forgot to fall back to the original default behaviour in
case the fix was not supported by the surrounding Python environment.

This should not cause any new pyreadline incompatibility issues because:
  1. fallback code does not use any Windows structure definitions that might
     conflict with those defined by pyreadline.
  2. pyreadline does not support Python 2.5.

colorama/win32.py
@@ -4,10 +4,9 @@
 STDOUT = -11
 STDERR = -12
 
-import ctypes
-from ctypes import LibraryLoader
-
 try:
+    import ctypes
+    from ctypes import LibraryLoader
     windll = LibraryLoader(ctypes.WinDLL)
     from ctypes import wintypes
 except (AttributeError, ImportError):