4804a4f39df82d089d27896d129b524981ed7cda
Bernardo Sulzbach <mafagafogigante@gmail.com>
2014-07-08 13:14:25 -0300

    demo02 improvements
    Tad shorter and clearer.
    Now it just calls print once.

--- demos/demo02.py
+++ demos/demo02.py
@@ -9,17 +9,14 @@ from colorama import init, Fore, Back, Style
 
 init()
 
-print(Fore.GREEN + 'green, '
-    + Fore.RED + 'red, '
-    + Fore.RESET + 'normal, '
-    , end='')
-print(Back.GREEN + 'green, '
-    + Back.RED + 'red, '
-    + Back.RESET + 'normal, '
-    , end='')
-print(Style.DIM + 'dim, '
-    + Style.BRIGHT + 'bright, '
-    + Style.NORMAL + 'normal'
-    , end=' ')
-print()
+colors = ('green', 'red', 'normal')
+styles = ('dim', 'bright', 'normal')
 
+fore = (Fore.GREEN, Fore.RED, Fore.RESET)
+back = (Back.GREEN, Back.RED, Back.RESET)
+style = (Style.DIM, Style.BRIGHT, Style.NORMAL)
+
+output = [fore[i] + colors[i] for i in range(3)]
+output.extend([back[i] + colors[i] for i in range(3)])
+output.extend([style[i] + styles[i] for i in range(3)])
+print(', '.join(output))
