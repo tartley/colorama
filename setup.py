7d0bd4e77b6bb26cbf7979d05a07459ae7bd521f
Simeon Visser <svisser@users.noreply.github.com>
2014-08-27 23:18:45 +0100

    Updated classifiers with supported Python versions

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Terminals',
     ]
 )
