99e27b4bdc896b61edba3d16439fa2f95d64ff9c
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-15 14:53:43 -0700

    Include PyPy in Travis testing
    Document its support as a trove classifier.

--- setup.py
+++ setup.py
@@ -54,12 +54,15 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Terminals',
     ]
 )
