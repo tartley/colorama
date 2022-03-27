19187216cb054bd2a073f2df3458fa5d52939993
Ofek Lev <ofekmeister@gmail.com>
2022-03-27 16:25:49 -0400

    Update package metadata

--- release.ps1
+++ release.ps1
@@ -1,7 +1,5 @@
 $ve="$HOME\.virtualenvs\colorama"
 $bin="$ve\Scripts"
-$version="$(& $bin\python.exe setup.py --version)"
 
 # Upload to PyPI.
-& $bin\twine.exe upload dist\colorama-$version*.tar.gz dist\colorama-$version-*.whl
-
+& $bin\twine.exe upload dist\colorama-*.tar.gz dist\colorama-*.whl
