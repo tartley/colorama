27eac48a395e9d2b93c6f0dab33a48f7d2c9772b
James Edington Administrator <codegeek98@gmail.com>
2025-06-10 15:14:19 -0500

    Windows 11: use x-microsoft-437 anyway
    Emoji being silently eaten by the CMD App window is arguably far
    better than killing the whole program because print() choked...

--- colorama/win32.py
+++ colorama/win32.py
@@ -205,7 +205,7 @@ else:
 
 # https://learn.microsoft.com/en-us/windows/win32/intl/code-page-identifiers
 MICROSOFT_CODEPAGE_ENCODING = {
-  437: 'x-windows-437' if not (sys.platform == 'win32' and sys.getwindowsversion().build >= 22000) else 'cp437',
+  437: 'x-windows-437',
 
   708: 'iso-8859-6',
   709: 'iso-9036',
